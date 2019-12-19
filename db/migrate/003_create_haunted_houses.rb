67330e1cb1c8c0130c83999d9513cffd21cdc42a
Kevin Downs <kevin.downs04@gmail.com>
2019-12-19 15:43:24 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
-# Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  
+    def change
+    create_table : do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme 
+      t.float :price 
+      t.boolean :family_friendly
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.text :description
+    end
+  end
+  
+end
